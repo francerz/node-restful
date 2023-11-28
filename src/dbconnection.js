e4b75e4ebb52693fe2587e963d5d2bd552b25020
Francerz <francerzam@hotmail.com>
2023-11-28 14:56:05 -0600

    Protected passwords by changing them by docker secrets

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -1,13 +1,20 @@
+const fs = require('fs');
 const knex = require('knex');
 
-const db = knex({
-    client: 'mysql2',
-    connection: {
-        host: 'mysql', // docker-compose service
-        user: 'user_biblioteca',
-        password: '54321',
-        database: 'biblioteca'
-    }
-});
+async function connectMysql() {
+    const mysqlPassword = await fs.promises.readFile('/run/secrets/mysql_password');
+    const db = knex({
+        client: 'mysql2',
+        connection: {
+            host: 'mysql', // docker-compose service
+            user: 'user_biblioteca',
+            password: mysqlPassword,
+            database: 'biblioteca'
+        }
+    });
+    return db;
+}
 
-module.exports = db;
+module.exports = {
+    connectMysql
+};
--- src/models/temas.js
+++ src/models/temas.js
@@ -1,11 +1,12 @@
-const db = require('../dbconnection');
-
+const { connectMysql } = require('../dbconnection');
 class TemasModel {
     static async consultar() {
+        let db = await connectMysql();
         let query = db('temas');
         return await query;
     }
     static async consultarPorId(id) {
+        let db = await connectMysql();
         return await db('temas').where('id_tema', id);
     }
 }
